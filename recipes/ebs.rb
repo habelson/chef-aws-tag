9f5bd026eaa6860691e893e89015c14752b370e1
habelson <heath.abelson@vistronix.com>
2015-05-07 16:52:46 -0400

    new try

--- recipes/ebs.rb
+++ recipes/ebs.rb
@@ -1,7 +1,16 @@
+::Chef::Recipe.send(:include, Opscode::Aws::Ec2)
+
 include_recipe "aws"
 
-chef-aws-tag_ebs_volume node['ec2']['instance_id'] do
-	action :describe
+unless node['aws-tag']['tags'].empty? || node['aws-tag']['tags'].nil?
+	vol = ec2.describe_volumes[:volumes].find do |v|
+		v[:attachments].any? { |a| a[:instance_id] == node['ec2']['instance_id'] }
+	end
+	
+    aws_resource_tag vol[:volume_id] do
+        tags(node['aws-tag']['tags'])
+        action :update
+    end
 end
 
 
