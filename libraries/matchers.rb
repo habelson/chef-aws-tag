b4b9a61733c5c8a426f2c87ec35a4ba68c6e0590
Paul Williams <williapv@gmail.com>
2015-05-12 10:03:22 -0500

    added matcher

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,6 +1,6 @@
 if defined?(ChefSpec)
   # ebs_raid
   def tag_instance_ebs_volumes(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:aws_tag_instance_volumes, :tag, resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:aws_tag_inst_vol, :tag, resource_name)
   end
 end
