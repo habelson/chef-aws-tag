2fb66c242e097657b2a26fb6d7c7bc32035fd025
Paul Williams <williapv@gmail.com>
2015-05-13 11:13:43 -0500

    updated tags

--- providers/ebs.rb
+++ providers/ebs.rb
@@ -16,11 +16,8 @@ end
 # Pulls the volume id from the volume_id attribute or the node data and verifies that the volume actually exists
 def determine_volume(instance_id)
   vol = currently_attached_volume(instance_id)
-  vol_id = (vol ? vol[:volume_id] : nil)
+  vol_id = (vol ? vol[:aws_id] : nil)
   fail 'volume_id attribute not set and no volume id is set in the node data for this resource (which is populated by action :create) and no volume is attached at the device' unless vol_id
-  # check that volume exists
-  vol = volume_by_id(vol_id)
-  fail "No volume with id #{vol_id} exists" unless vol
   vol
 end
 
