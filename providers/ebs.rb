d9c6ee523dd18a3d1ecd8f63b51ae57f7c0619e9
Paul Williams <williapv@gmail.com>
2015-05-13 09:29:21 -0500

    added some debugging

--- providers/ebs.rb
+++ providers/ebs.rb
@@ -30,7 +30,9 @@ end
 
 # Returns the volume that's attached to the instance at the given device or nil if none matches
 def currently_attached_volume(instance_id)
+  Chef::Log.info "Getting currently attached volumes for instance #{instance_id}"
   ec2.describe_volumes[:volumes].find do |v|
+    Chef::Log.info "volume info #{v}"
     v[:attachments].any? { |a| a[:instance_id] == instance_id }
   end
 end
