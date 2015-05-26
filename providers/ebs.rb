086ed28db010c37ea92369cc6d7bb012d416ac63
Paul Williams <williapv@gmail.com>
2015-05-26 10:27:49 -0500

    first draft to support multiple drives

--- providers/ebs.rb
+++ providers/ebs.rb
@@ -3,26 +3,21 @@ include Opscode::Aws::Ec2
 
 #only does one volume for the instance right now.  TODO:Update to make work with multiples
 action :tag do
-    	ebs_vol = determine_volume(node['ec2']['instance_id'])
-    	aws_resource_tag 'chef generated volume' do
-    	resource_id ebs_vol[:aws_id]
-        tags(node['aws-tag']['tags'])
-        action :update
-	end
-end
-
-# Pulls the volume id from the volume_id attribute or the node data and verifies that the volume actually exists
-def determine_volume(instance_id)
-  vol = currently_attached_volume(instance_id)
-  vol_id = (vol ? vol[:aws_id] : nil)
-  fail 'volume_id attribute not set and no volume id is set in the node data for this resource (which is populated by action :create) and no volume is attached at the device' unless vol_id
-  vol
+    	ebs_vols = currently_attached_volumes(node['ec2']['instance_id'])
+    	ebs_vols.each do | ebs_vol |
+    		aws_resource_tag 'chef generated volume' do
+    			resource_id ebs_vol[:aws_id]
+        		tags(node['aws-tag']['tags'])
+        		action :update
+			end
+		end
 end
 
 # Returns the volume that's attached to the instance at the given device or nil if none matches
-def currently_attached_volume(instance_id)
-  ec2.describe_volumes.find do |v|
-    v[:aws_instance_id] == instance_id
-  end
+def currently_attached_volumes(instance_id)
+  ec2.describe_volumes.select {|v| v[:aws_instance_id] == instance_id}
+  #ec2.describe_volumes.find do |v|
+  #  v[:aws_instance_id] == instance_id
+  #end
 end
 
